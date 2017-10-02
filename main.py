b0ad08579d5ccfbcb72f22429493fdbdfe1ddb8a
ask2696 <ask_2696@gmail.com>
2017-10-02 22:18:14 +0530

    Interface for all the functions

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from libraries_btTDD import *
 #truth_table = np.array([[0,2,1],[1,1,1],[0,2,1]])
 #truth_table = np.array([[0,1,1],[0,1,1],[0,1,1]])
 
-truth_table = np.array([[0,1,2],[1,2,0],[2,0,1]]) #HA
+#truth_table = np.array([[0,1,2],[1,2,0],[2,0,1]]) #HA
 
 #truth_table = np.array([[0,1,2],[0,1,2],[0,1,2]])
 
-#truth_table = np.array([[0,1,2],[1,2,0],[1,2,0]])
+truth_table = np.array([[0,1,2],[1,2,0],[1,2,0]])
 
 #truth_table = np.array([[1,1,1],[1,1,1],[1,1,1]])
 
@@ -22,6 +22,8 @@ print "1| %d  %d  %d" %(truth_table[1,0],truth_table[1,1],truth_table[1,2])
 print "2| %d  %d  %d\n" %(truth_table[2,0],truth_table[2,1],truth_table[2,2])
 
 out_btTdd,null = decision_template(truth_table)
+
+cost_vector,tc,init_cost_table = transistor_count(out_btTdd)
 
 print_btTDD(out_btTdd)
 
